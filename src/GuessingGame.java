995b69236af30f7ad59677fad148f6c6672793a3
DJRoche509 <davjroche@gmail.com>
2023-05-31 21:25:03 -0500

    Created constructor and main. Plus, greet user

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,2 +1,12 @@
 public class GuessingGame {
+    public GuessingGame(){
+    }
+
+    public static void main(String[] args) {
+        String greet = "Howdy, what's your name?";
+        // Greet the user
+        System.out.println("Java Game\n"+greet);
+
+
+    }
 }
